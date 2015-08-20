ae38c1355574005f90c9cade9c7f7004e1e6e1b5
Ciaran McGauran <mcgauranc@gmail.com>
2015-08-21 00:21:37 +0100

    more  animation changes
    more  animation changes

--- public/js/core.js
+++ public/js/core.js
@@ -1,43 +1,38 @@
-var myCareer = angular.module('myCareer', ['ngMap']);
-var loader = function(){
-	$(".timeline-panel").each(function(){
- 
-		if (isScrolledIntoView(this) === true) {
-			$(this).addClass('timeline-panel-anim').parent().addClass('visible');
-		} else  {
-			console.log("not adding class on " + $(this).parent().index())
-			$(this).removeClass('timeline-panel-anim').parent().removeClass('visible');
-		}
-	});
-	
-}
-$(document).ready(loader())
-$(window).scroll(
-	function(){
-		console.log(this)
-		loader();
-	}
-);
-function isScrolledIntoView(elem) {
-	var docViewTop = $(window).scrollTop();
-	var docViewBottom = docViewTop + $(window).height();
-	var elemTop = $(elem).offset().top;
-	var elemBottom = elemTop + $(elem).height();
-
-	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
+var myCareer = angular
+    .module('myCareer', ['ngMap', 'ngRoute', 'ngResource'])
+    .controller("mainController", mainController)
+    .directive('onFinishRender',['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit('ngRepeatFinished');
+                        if(!!attr.onFinishRender){
+                          $parse(attr.onFinishRender)(scope);
+                        }
+                    });
+                }
+            }
+        }
+    }]);
+mainController.$inject = ["$scope", "$http"];
 function mainController($scope, $http) {
     $scope.formData = {};
-    $scope.timeSpan = function(from,to){
-		return moment(from).format("MMM YY") + " to " +  moment(to).format("MMM YY");
-	};
-	$scope.stripCo = function(str) {
-		return str.replace(/\s/g,'');
-	}
-    $scope.diffDate = function(from,to){
-		now = moment(to)
-		return Math.round(now.diff(moment(from), 'months', true)/ 12);
-	};
+    $scope.loader = function(){
+        loader();
+        $("html").removeClass("loading");
+    }
+    $scope.timeSpan = function(from, to) {
+        return moment(from).format("MMM YY") + " to " + moment(to).format("MMM YY");
+    };
+    $scope.stripCo = function(str) {
+        return str.replace(/\s/g, '');
+    }
+    $scope.diffDate = function(from, to) {
+        now = moment(to)
+        return Math.round(now.diff(moment(from), 'months', true) / 12);
+    };
     // when landing on the page, get all todos and show them
     $http.get('/data/roles')
         .success(function(data) {
@@ -53,7 +48,6 @@ function mainController($scope, $http) {
             .success(function(data) {
                 $scope.formData = {}; // clear the form so our user is ready to enter another
                 $scope.todos = data;
-                console.log(data);
             })
             .error(function(data) {
                 console.log('Error: ' + data);
@@ -72,4 +66,62 @@ function mainController($scope, $http) {
             });
     };
 
+}
+myCareer.factory('myHttpInterceptor', function($q, $window) {
+    return function(promise) {
+        return promise.then(function(response) {
+            $("#spinner").hide();
+            return response;
+        }, function(response) {
+            $("#spinner").hide();
+            return $q.reject(response);
+        });
+    };
+});
+myCareer.config(function($httpProvider) {
+    $httpProvider.interceptors.push('myHttpInterceptor');
+    var spinnerFunction = function spinnerFunction(data, headersGetter) {
+        $("#spinner").show();
+        return data;
+    };
+    $httpProvider.defaults.transformRequest.push(spinnerFunction);
+});
+
+var loader = function() {
+    $(".timeline-panel").each(function() {
+        var first = false;
+        if (!first && isScrolledIntoView(this) === true) {
+            if (!first && !$(this).hasClass('timeline-panel-first')) {
+                $(this).addClass('timeline-panel-first');
+                first = true;
+                           
+            } else if($(this).hasClass('timeline-panel-first')) {
+                    $(this).removeClass('timeline-panel-first');                                
+            }
+            if(!$(this).hasClass('timeline-panel-anim')) {
+                $(this).addClass('timeline-panel-anim').parent().addClass('visible');
+            }
+        } else {
+            $(".reveal", this).removeClass(".reveal").addClass(".reveal");
+            $(this).removeClass('timeline-panel-anim').parent().removeClass('visible');
+            if($(this).hasClass('timeline-panel-first')) {
+                $(this).removeClass('timeline-panel-first');
+            }                                
+
+        }
+    });
+
+}
+$(window).scroll(
+    function() {
+        loader();
+    }
+);
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
