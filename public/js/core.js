d60db8405dfc2f153bf38bb996f8e1a9c68cc9f9
unknown <ciaran.mcgauran@SAGE014486.sageukie.adinternal.com>
2015-08-20 16:44:47 +0100

    Animations

--- public/js/core.js
+++ public/js/core.js
@@ -1,23 +1,47 @@
 var myCareer = angular.module('myCareer', ['ngMap']);
+var loader = function(){
+	$(".timeline-panel").each(function(){
  
+		if (isScrolledIntoView(this) === true) {
+			$(this).addClass('timeline-panel-anim').parent().addClass('visible');
+		} else  {
+			console.log("not adding class on " + $(this).parent().index())
+			$(this).removeClass('timeline-panel-anim').parent().removeClass('visible');
+		}
+	});
+	
+}
+$(document).ready(loader())
+$(window).scroll(
+	function(){
+		console.log(this)
+		loader();
+	}
+);
+function isScrolledIntoView(elem) {
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
+	var elemTop = $(elem).offset().top;
+	var elemBottom = elemTop + $(elem).height();
+
+	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
 function mainController($scope, $http) {
     $scope.formData = {};
     $scope.timeSpan = function(from,to){
 		return moment(from).format("MMM YY") + " to " +  moment(to).format("MMM YY");
 	};
 	$scope.stripCo = function(str) {
-		console.log(str.replace(/\s/g,''))
 		return str.replace(/\s/g,'');
 	}
     $scope.diffDate = function(from,to){
-		now =moment(to)
+		now = moment(to)
 		return Math.round(now.diff(moment(from), 'months', true)/ 12);
 	};
     // when landing on the page, get all todos and show them
     $http.get('/data/roles')
         .success(function(data) {
             $scope.todos = data;
-            console.log(data);
         })
         .error(function(data) {
             console.log('Error: ' + data);
