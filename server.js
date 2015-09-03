846c7b9ab24a51473d6b238f93c1c7904f38e0cd
unknown <ciaran.mcgauran@SAGE014486.sageukie.adinternal.com>
2015-09-03 11:15:55 +0100

    db change

--- server.js
+++ server.js
@@ -22,5 +22,14 @@
     app.get('*', function(req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
-    app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3100);
-    console.log("App listening on port 3100");
+	var http = require('http');
+
+	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+	app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+
+	http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+		console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+		server();
+	});
+    console.log("App listening");
